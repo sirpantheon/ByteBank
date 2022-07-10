5413c460a736b505f58fb6b4b201b5570e3fddbe
johnatan sousa paixao <johnatan.sousa@gmail.com>
2022-07-09 22:56:04 -0300

    Initial commit

---
+++ ByteBank/ContaCorrente.cs
@@ -0,0 +1,12 @@
+﻿
+namespace ByteBank{
+
+    public class ContaCorrente
+    {
+        public string titular;
+        public string conta;
+        public int numero_agencia;
+        public string nome_agencia;
+        public double saldo;
+    }
+}
---
+++ ByteBank/Program.cs
@@ -0,0 +1,17 @@
+﻿using ByteBank;
+Console.WriteLine("Bem Vindo ao Seu Banco, ByteBank");
+
+ContaCorrente conta1 = new ContaCorrente();
+
+conta1.titular = "Johnatan Paixão";
+conta1.conta = "";
+conta1.numero_agencia = 134678;
+conta1.nome_agencia = "Banco Central";
+conta1.saldo = 134.6;
+
+Console.WriteLine("Titular: " + conta1.titular); ;
+Console.WriteLine("Conta: " + conta1.conta); ;
+Console.WriteLine("numero_agencia: " + conta1.numero_agencia);
+Console.WriteLine("nome_agencia" + conta1.nome_agencia);
+Console.WriteLine("saldo: " + conta1.saldo);
+Console.ReadKey();
