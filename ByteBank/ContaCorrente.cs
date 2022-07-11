dcdc3ec97764b0828b5b979880477a0262a7bd7f
johnatan sousa paixao <johnatan.sousa@gmail.com>
2022-07-11 20:15:50 -0300

    Depositar & Tranferir

--- ByteBank/ContaCorrente.cs
+++ ByteBank/ContaCorrente.cs
@@ -1,9 +1,11 @@
 ﻿
+using ByteBank.Titular;
+
 namespace ByteBank{
 
     public class ContaCorrente
     {
-        public string titular;
+        public Cliente titular;
         public string conta;
         public int numero_agencia;
         public string nome_agencia;
@@ -14,12 +16,50 @@ namespace ByteBank{
         {
             return false;
         }
+            if (valor < 0)
+            {
+                return false;
+            }
         else
         {
             saldo = saldo - valor;
             return true;
         }
     }
+    public void Depositar(double valor)
+    {
+            saldo = saldo + valor;
     }
+
+    public bool Tranferir(double valor, ContaCorrente destino)
+        {
+            if(saldo < valor)
+            {
+                return false;
+            }
+            if (valor < 0)
+            {
+                return false;
+            }
+            else
+            {
+                saldo = saldo - valor;
+                destino.saldo =  destino.saldo - valor;
+                return true;
+            }
+        }
+
+    public void ExibirDadosDaConta()
+        {
+            Console.WriteLine("Titular: " + titular);
+            Console.WriteLine("Conta: " + conta);
+            Console.WriteLine("Número Agencia: " + numero_agencia);
+            Console.WriteLine("Nome Agência: " + nome_agencia);
+            Console.WriteLine("saldo: " + saldo);
+        }
+
+
+    }
+
 
 }
--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -1,23 +1,26 @@
 ﻿using ByteBank;
 Console.WriteLine("Bem Vindo ao Seu Banco, ByteBank");
 
-ContaCorrente conta1 = new ContaCorrente();
-ContaCorrente conta2 = new ContaCorrente();
+//ContaCorrente conta1 = new ContaCorrente();
+//ContaCorrente conta2 = new ContaCorrente();
 
-conta1.titular = "Johnatan Paixão";
-conta1.conta = "654321";
-conta1.numero_agencia = 134678;
-conta1.nome_agencia = "Banco Central";
-conta1.saldo = 500.12;
+//conta1.conta = "654321";
+//conta1.numero_agencia = 134678;
+//conta1.nome_agencia = "Banco Central";
+//conta1.saldo = 500.12;
 
-conta2.titular = "Narjara Paixão";
-conta2.conta = "321654";
-conta2.numero_agencia = 134678;
-conta2.nome_agencia = "Banco Central";
-conta2.saldo = 134.6;
+//conta2.conta = "321654";
+//conta2.numero_agencia = 134678;
+//conta2.nome_agencia = "Banco Central";
+//conta2.saldo = 134.6;
 
-Console.WriteLine("Saldo Do Johnatan: " + conta1.saldo);
-conta1.Sacar(33);
-Console.WriteLine("Saldo Do Johnatan: " + conta1.saldo);
+//Console.WriteLine("Saldo Do Johnatan: " + conta1.saldo);
+//conta1.Sacar(33);
+//Console.WriteLine("Saldo Do Johnatan: " + conta1.saldo);
+
+Cliente cliente = new Cliente();
+cliente.nome = "johnatan";
+cliente.cpf = "56489732187";
+cliente.profissao = "vigia";
 
 Console.ReadKey();
