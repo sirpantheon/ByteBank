93ed2a1152034d02e7e5c0a4db915af60c5a5ee2
johnatan sousa paixao <johnatan.sousa@gmail.com>
2022-07-12 14:28:00 -0300

    fn

--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -3,9 +3,9 @@
 
 Console.WriteLine("Bem Vindo ao Seu Banco, ByteBank");
 
-ContaCorrente conta1 = new ContaCorrente(321, "321564-x", "johnatan", "321654987-65");
-ContaCorrente conta2 = new ContaCorrente(653, "326569-x", "paulo", "568459326-65");
-ContaCorrente conta3 = new ContaCorrente(985,"654987-x","hugo","654987326-65");
+ContaCorrente conta1 = new ContaCorrente(321, "321564-x");
+ContaCorrente conta2 = new ContaCorrente(653, "326569-x");
+ContaCorrente conta3 = new ContaCorrente(985,"654987-x");
 
 Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
 
